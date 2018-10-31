456db30017de367a8441b45d5ba7a90613238b1d
Manish sahani <manishsahani1351111@gmail.com>
2018-10-31 15:15:08 +0530

    Password Recover done

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<section class="cover cover-fullscreen">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-8">
+                <h1>Recover Password</h1>
+                <form action="{{ route('password.email') }}" method="post" class="row">
+                    @csrf
+                    <div class="col-md-12">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="" class="class-validate">
+                    </div>
+                    <div class="col-md-5">
+                        <button type="submit" class="btn btn--sm">Send Password reset link</button>
+                    </div>
+                </form>
+                <br>
+                <p>
+                    <small>if you are still facing problem, mail us at dtutimes@dtu.ac.in.</small>
+                </p>
             </div>
         </div>
     </div>
-</div>
+</section>
+
 @endsection
