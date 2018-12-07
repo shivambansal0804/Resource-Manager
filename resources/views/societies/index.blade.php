274e7f7bf31eef653b120755e748e777b6a0c9e2
Manish sahani <manishsahani1351111@gmail.com>
2018-12-07 18:58:10 +0530

    Still resolving jpg

--- resources/views/societies/index.blade.php
+++ resources/views/societies/index.blade.php
@@ -61,21 +61,6 @@
 							</div>
 						</article>
 
-						<article class="portfolio-item pf-media pf-tech">
-							<div class="portfolio-image">
-								<a href="societies/single/asme">
-									<img src="img/society/asme/1.jpg" alt="asme">
-								</a>
-								<div class="portfolio-overlay">
-									<a href="img/society/asme/1.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-									<a href="societies/single/asme" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-								</div>
-							</div>
-							<div class="portfolio-desc" style="text-align: center;">
-								<h3 style="display: inline-block;"><a href="societies/single/asme">ASME</a></h3>
-							</div>
-						</article>
-
 						<article class="portfolio-item pf-media pf-misc">
 							<div class="portfolio-image">
 								<a href="societies/single/assets">
@@ -223,21 +208,6 @@
 							</div>
 							<div class="portfolio-desc" style="text-align: center;">
 								<h3 style="display: inline-block;"><a href="societies/single/dtubhangra">DTU Bhangra</a></h3>
-							</div>
-						</article>
-
-						<article class="portfolio-item pf-media pf-misc">
-							<div class="portfolio-image">
-								<a href="societies/single/ecell">
-									<img src="img/society/ecell/1.png" alt="ecell">
-								</a>
-								<div class="portfolio-overlay">
-									<a href="img/society/ecell/1.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-									<a href="societies/single/ecell" class="right-icon"><i class="icon-line-ellipsis"></i></a>
-								</div>
-							</div>
-							<div class="portfolio-desc" style="text-align: center;">
-								<h3 style="display: inline-block;"><a href="societies/single/ecell">E-Cell DTU</a></h3>
 							</div>
 						</article>
 
