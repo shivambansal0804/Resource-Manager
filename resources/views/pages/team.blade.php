80290a12fef2ab85339e684163576f671dee16eb
Manish sahani <manishsahani1351111@gmail.com>
2018-12-07 19:27:52 +0530

    Manage Dash changed

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -1197,31 +1197,6 @@
 							</div>
 						</div>
 
-						<div class="col-md-2 col-xs-6 bottommargin">
-							<div class="team">
-								<div class="team-image">
-									<img src="img/team/Columnists/Saurabh%20Tiwari.jpeg" alt="Saurabh Tiwari">
-								</div>
-								<div class="team-desc">
-									<div class="team-title">
-										<h5>Saurabh <br>Tiwari</h5>
-									</div>
-									<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/stthegreat
-" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-										<i class="icon-facebook"></i>
-										<i class="icon-facebook"></i>
-									</a>
-									<a target="_blank" rel="noopener noreferrer" href="#
-" class="social-icon inline-block si-small si-light si-rounded si-linkedin">
-										<i class="icon-linkedin"></i>
-										<i class="icon-linkedin"></i>
-									</a>
-
-
-
-								</div>
-							</div>
-						</div>
 
 						<div class="col-md-2 col-xs-6 bottommargin">
 							<div class="team">
