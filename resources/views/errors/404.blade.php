504a2389ccccf0dd23f1e430f7d5e3930d757cab
Manish sahani <manishsahani1351111@gmail.com>
2018-12-24 10:58:47 +0530

    Errors image add

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -21,7 +21,7 @@
 			<div class="container pos-vertical-center">
 				<div class="row">
 					<div class="col-md-12">
-						<img src="{{ asset('img/logofav.1.png')}}" alt="">
+						<img src="{{ asset('img/logofav1.png')}}" alt="">
 						<h1 class="h1--large">404</h1>
 						<p class="lead">
 							Not Found
