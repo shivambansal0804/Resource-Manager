392cb0e23af0669622996507362b7ddf7cd50bde
Manish sahani <manishsahani1351111@gmail.com>
2018-12-24 12:45:46 +0530

    Superuser dashboard new

--- resources/views/users/superuser/index.blade.php
+++ resources/views/users/superuser/index.blade.php
@@ -1,15 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
+    <section class="pt-5 pb-5 border--bottom">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-md-8 mt-md-5 text-center">
-                    <div class="mt-3">
-                        <img src="{{ asset('svg/admin.svg')}}" alt="" srcset="" width="250">
-                    </div>
-                    <br>
-                    <h2>Welcome {{ auth()->user()->name }}</h2>
+                <div class="col-md-12 col-lg-10">
+                    <h1>Hi, {{ auth()->user()->name }}</h1>
+                    <p>
+                        Welcome to the backdoor for the DTU Times website.
+                    </p>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <section class="pt-5 pb-0">
+        <div class="container">
+            <h3 class="ml-5">Users</h3>
+            <div class="row text-center"> 
+                @php
+                    $photographer = \App\Role::where('name', 'photographer')->with('users')->first()->users->count();
+                    $columnists = \App\Role::where('name', 'columnist')->with('users')->first()->users->count();
+                    $councils = \App\Role::where('name', 'council')->with('users')->first()->users->count();
+                @endphp  
+                <div class="col-md-2 offset-md-1">
+                    <img src="{{ asset('svg/team.svg') }}" alt="" class="image--sm">
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ \App\User::count() }}</h1>
+                    <small>Users</small>
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ $columnists }}</h1>
+                    <small>Columnists</small>
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ $photographer }}</h1>
+                    <small>Photographers</small>
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ $councils }}</h1>
+                    <small>Council</small>
                 </div>
             </div>
         </div>
