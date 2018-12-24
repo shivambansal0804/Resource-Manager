21221648dffa18d689fe293d55a6c3a0f9eff6e0
Manish sahani <manishsahani1351111@gmail.com>
2018-12-24 10:59:52 +0530

    Errors image add

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -21,7 +21,7 @@
 			<div class="container pos-vertical-center">
 				<div class="row">
 					<div class="col-md-12">
-						<img src="{{ asset('img/logofav1.png')}}" alt="">
+						<img class="image--lg" src="{{ asset('img/logofav1.png')}}" alt="">
 						<h1 class="h1--large">403</h1>
 						<p class="lead">
 							Permission denied
