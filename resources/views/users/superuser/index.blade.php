b6d90b4373c82f6b14246c3ab7e863009f7c415c
Manish sahani <manishsahani1351111@gmail.com>
2018-12-24 13:03:08 +0530

    New Changes in Superuser Dashboard

--- resources/views/users/superuser/index.blade.php
+++ resources/views/users/superuser/index.blade.php
@@ -15,9 +15,17 @@
         </div>
         <!--end of container-->
     </section>
-    <section class="pt-5 pb-0">
+
+    <section class="space--sm border--bottom">
         <div class="container">
-            <h3 class="ml-5">Users</h3>
+            <div class="row justify-content-center">
+                <div class="col-md-12 col-lg-10">
+                    <div class="cta">
+                        <h2>Users</h2>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
             <div class="row text-center"> 
                 @php
                     $photographer = \App\Role::where('name', 'photographer')->with('users')->first()->users->count();
@@ -45,5 +53,41 @@
                 </div>
             </div>
         </div>
+        <!--end of container-->
+    </section>
+
+    <section class="space--sm">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 col-lg-10">
+                    <div class="cta">
+                        <h2>Stories</h2>
+                    </div>
+                </div>
+            </div>
+            <!--end of row-->
+            <div class="row text-center">  
+                <div class="col-md-2 offset-md-1">
+                    <img src="{{ asset('svg/type.svg') }}" alt="" class="image--sm">
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ \App\Models\Story::count() }}</h1>
+                    <small>Total</small>
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ \App\Models\Story::where('status', 'published')->count() }}</h1>
+                    <small>Published</small>
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ \App\Models\Story::where('status', 'pending')->count() }}</h1>
+                    <small>Pending</small>
+                </div>
+                <div class="col-md-2">
+                    <h1 class="mb-0">{{ \App\Models\Story::where('status', 'draft')->count() }}</h1>
+                    <small>Draft</small>
+                </div>
+            </div>
+        </div>
+        <!--end of container-->
     </section>
 @endsection
