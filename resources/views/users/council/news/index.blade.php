18cf66e210012c7eb03e9424d599ccc25c8c4f48
manishsahani999 <manishsahani1351111@gmail.com>
2019-04-07 17:37:16 +0530

    edit news done

--- resources/views/users/council/news/index.blade.php
+++ resources/views/users/council/news/index.blade.php
@@ -41,9 +41,10 @@
                             <small>
                                 Created <strong>{{ $item->created_at->diffForHumans() }}</strong>
                             </small>
-                            @if (Route::currentRouteName() != 'council.societies.news.index')
+                            
                             <p class="mt-2">
 
+                            @if (Route::currentRouteName() != 'council.societies.news.index')
                                 @if (Route::currentRouteName() == 'council.societies.news.pending')
                                 <a class="text-success type--uppercase" href="{{route('council.societies.news.publish', $item->uuid)}}">
                                     <small>Publish</small>
@@ -60,13 +61,16 @@
                                     <small>Edit</small>
                                 </a>
 
+                                
+                            @endif
+
                                 <a class="text-danger type--uppercase" href="" onclick="event.preventDefault();
                                     document.getElementById('delete-form').submit();">
                                     <small>Delete</small>
                                 </a>
                                 <form id="delete-form" action="{{route('council.societies.news.destroy', $item->uuid)}}" method="post">@csrf @method('DELETE')</form>
+                                
                             </p>
-                            @endif
                         </div>
                         <div class="col-md-4">
                             <img alt="Image" class="border--round" src="{{ $item->getFirstMediaUrl('soc_news_images') }}" />
