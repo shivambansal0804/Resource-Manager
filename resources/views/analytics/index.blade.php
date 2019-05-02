095507698c42bef90467823033a2e1c0f99b31d5
manishsahani999 <manishsahani1351111@gmail.com>
2019-05-02 16:18:03 +0530

    test

--- resources/views/analytics/index.blade.php
+++ resources/views/analytics/index.blade.php
@@ -26,7 +26,7 @@
                             <div class="col-md-8">
                                 <h5 class="mb-0">{{$story->title}}</h5>
                                 <span><small>Story by {{$story->user->name }}</small></span>
-                                <p><a  href="{{route('blog.show', $item->slug)}}">Read</a></p>
+                                <p><small><a href="{{route('blog.show', $story->slug)}}">Read</a></small></p>
                             </div>
                             <div class="col-md-4 text-center">
                                 <h2>{{$item['views']}}</h2>
